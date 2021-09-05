459a6457bcf7797d5f74ce54e7b7c50fbe372958
AndreaEv18 <andrea.marrocco95@gmail.com>
2021-09-05 16:30:43 +0200

    Registra fix

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def datas():
         vini[nomeVino].append(prezzoVendita)
         vini[nomeVino].append(str(contatore))
         vini[nomeVino].append(user)
-        return render_template('catalogo.html', vino=vini)
+        return render_template('catalogo.html', vino=vini, utente=user, flag = 1 )
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8115)
